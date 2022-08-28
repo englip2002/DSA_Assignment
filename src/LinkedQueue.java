fe27a478d60ce29991dd30e1f6c5ea14e3826b1c
AlpacaMoon <thongxsgaming@gmail.com>
2022-08-28 20:34:10 +0800

    Alpaca#1

---
+++ src/LinkedQueue.java
@@ -0,0 +1,119 @@
+
+/**
+ *
+ * @author Thong So Xue
+ */
+
+import java.util.Iterator;
+import java.io.Serializable;
+import java.util.List;
+
+public class LinkedQueue<T> implements QueueInterface<T>, Serializable {
+    // Class attributes
+    private Node firstNode, lastNode;
+    
+    
+    // Private Node class
+    private class Node implements Serializable {
+        private T data;
+        private Node next;
+        
+        // Node constructor
+        public Node(T newEntry) {
+            this.data = newEntry;
+            this.next = null;
+        }
+    }
+    
+    
+    // Constructors
+    public LinkedQueue() {
+        firstNode = null;
+        lastNode = null;
+    }
+    
+    // Methods
+    @Override
+    public void enqueue(T newEntry) {
+        Node newNode = new Node(newEntry);
+        if (isEmpty()) {
+            // Point firstNode and lastNode to the only node
+            firstNode = newNode;
+            lastNode = newNode;
+        }
+        else {
+            // Append newNode to the end of the queue
+            lastNode.next = newNode;
+            lastNode = newNode;
+        }
+    }
+
+    @Override
+    public T dequeue() {
+        // Return error if there are no elements to dequeue
+        if (isEmpty()) {
+            return (T) "The LinkedQueue is empty, no data to dequeue. ";
+        }
+        
+        // Keep reference of the data to be returned
+        T output = firstNode.data;
+        
+        // Update firstNode (and lastNode if queue is empty after dequeue operation)
+        firstNode = firstNode.next;
+        if (isEmpty()) {
+            lastNode = null;
+        }
+        
+        return output;
+    }
+
+    @Override
+    public T getFront() {
+        return firstNode.data;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // Only check if firstNode is null because 
+        // if firstNode is null, then lastNode must also be null
+        return firstNode == null;
+    }
+
+    @Override
+    public void clear() {
+        firstNode = null;
+        lastNode = null;
+    }
+    
+    // Iterator class
+    private class LinkedQueueIterator implements Iterator<T> {
+        private Node iterNode;
+        
+        public LinkedQueueIterator() {
+            iterNode = firstNode;
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return iterNode != null;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext()) {
+                T output = iterNode.data;
+                iterNode = iterNode.next;
+                return output;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+    
+    // Iterator
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedQueueIterator();
+    }
+}
---
+++ src/QueueInterface.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Thong So Xue
+ */
+
+import java.lang.Iterable;
+
+public interface QueueInterface<T> extends Iterable<T> {
+    
+    public void enqueue(T newEntry);
+    public T dequeue();
+    public T getFront();
+    public boolean isEmpty();
+    public void clear();
+}
