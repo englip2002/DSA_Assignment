b4c70f076174785e0751a1df966cfaf40b7ef283
zhilin1112 <87698109+zhilin1112@users.noreply.github.com>
2022-08-30 00:03:54 +0800

    updated

---
+++ Category.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+/**
+ *
+ * @author User
+ */
+public class Category {
+    private ArraySet<MenuItem> menuItems;
+    private char categoryName;
+    private static int counter = 0;
+
+    public Category(char categoryName) {
+        this.categoryName = categoryName;
+        menuItems = new ArraySet<MenuItem>();
+    }
+
+    public char getCategoryName() {
+        return categoryName;
+    }
+
+    public ArraySet<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+        counter++;
+    }
+
+    public void removeMenuItem(MenuItem menuItem) {
+        menuItems.remove(menuItem);
+        counter--;
+    }
+
+    //index = index of dishes in category
+    //dish = new modified dishes
+    public boolean modifyMenuItem(MenuItem menuItem, int index) {
+        menuItems.remove(menuItems.getElement()[index - 1]);
+        menuItems.add(menuItem);
+        return true;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+}
---
+++ MenuItem.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+/**
+ Author: KONG ZHI LIN
+ Description: Store food and beverage 
+ */
+public class MenuItem {
+    
+    //Properties
+    private char dishCategory; //M = main course , B = beverage, A = Appertizer, D = Dessert
+    private String dishName;
+    private String dishID; 
+    private double dishPrice;
+    private String dishDescription;
+    private static int dishCounter = 1; //  Number to improve the index
+    
+    //Constructor
+    public MenuItem(){
+        this.dishCategory = ' ';
+        this.dishName = "";
+        this.dishPrice = 0.0;
+        this.dishDescription = "";
+        this.dishID = "";
+    }
+    
+    public MenuItem(char dishCategory, String dishName, double dishPrice, String dishDescription){
+        this.dishCategory = dishCategory;
+        this.dishName = dishName;
+        this.dishPrice = dishPrice;
+        this.dishDescription = dishDescription;
+        this.dishID = generateDishID();
+        dishCounter++;
+    }
+
+    public MenuItem(String dishID, char dishCategory, String dishName, double dishPrice, String dishDescription){
+        this.dishCategory = dishCategory;
+        this.dishName = dishName;
+        this.dishPrice = dishPrice;
+        this.dishDescription = dishDescription;
+        this.dishID = dishID;
+        dishCounter++;
+    }
+    
+    //Accessors & Mutators 
+    
+     public String getDishName(){
+        return dishName;
+    }
+    
+    public double getDishPrice(){
+        return dishPrice;
+    }
+    
+    public String getFoodDescription(){
+        return dishDescription;
+    }
+    
+    public char getDishCategory(){
+        return dishCategory;
+    }
+
+    public String getDishID() {
+        return dishID;
+    }
+    
+    public void setDishName(String dishName){
+        this.dishName = dishName;
+    }
+    
+    public void setDishPrice(double dishPrice){
+        this.dishPrice = dishPrice;
+    }
+    
+    public void setDishDescription(String dishDescription){
+        this.dishDescription = dishDescription;
+    }
+    
+    public void setDishCategory(char dishCategory){
+        this.dishCategory = dishCategory;
+    }
+    
+    private String generateDishID(){
+        switch (dishCategory) {
+            case 'A':
+                return String.format("A%5d", dishCounter);
+            case 'M':
+                return String.format("M%5d", dishCounter);
+            case 'B':
+                return String.format("B%5d", dishCounter);
+            default:
+                return String.format("D%5d", dishCounter);
+        }
+    }
+    
+    public String toString(){
+        return "Dish ID           : " + dishID + "\n" +
+               "Dish Name         : " + dishName + "\n" + 
+               "Dish Price        : " + dishPrice + "\n" + 
+               "Dish Description  : " + dishDescription + "\n";
+    }
+    
+
+}
