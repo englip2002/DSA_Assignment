2bc3bf2728d5e09f0a2683a72567ae81fc38bbb8
EngLip <englip455@gmail.com>
2022-09-06 22:42:07 +0800

    x

--- src/Payment.java
+++ src/Payment.java
@@ -2,9 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package defaultPackage;
-
-
 import static java.lang.Math.*;
 
 import java.util.Scanner;
